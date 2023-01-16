96e5f7d0f8d844ebd5bd78b62395ec887e5adf3e
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-01-16 11:27:05 +0900

    feat: 서버로부터 데이터 받기

--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -7,3 +7,9 @@ export const csr = dev;
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+export async function load() {
+  let response = await fetch("https://dummyjson.com/products/1")
+  let result = await response.json()
+  return result
+}
